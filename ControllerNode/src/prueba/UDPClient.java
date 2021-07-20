9aac6184235dca79cee476d3bce2f819724bd891
maikelmatamoros <37597397+maikelmatamoros@users.noreply.github.com>
2021-07-20 14:49:25 -0600

    Recuperación de archivos desde los discos en ControllerNode
    Co-Authored-By: MabelHeller <28734060+MabelHeller@users.noreply.github.com>

--- ControllerNode/src/prueba/UDPClient.java
+++ ControllerNode/src/prueba/UDPClient.java
@@ -13,30 +13,65 @@ import java.util.Scanner;
 import static prueba.UDPService.data;
 
 public class UDPClient {
-
+    static int[] puertosDiscos;
     public static void main(String[] args) {
+
         boolean isNot = true;
-        int[] puertosDiscos = {8877,8878,8879,8880};
+        
         try {
-            
+
             DatagramSocket socketS = new DatagramSocket(8888);
             InetAddress adress = InetAddress.getByName("localhost");
-            for(int i=0 ;i< puertosDiscos.length;i++){
-               byte[] data2 = String.valueOf(puertosDiscos[i]).getBytes();
-               System.out.println("Client Port:" + puertosDiscos[i]);
-               socketS.send(new DatagramPacket(data2, data2.length, adress, 7777));
-               byte[] receive = new byte[65535];
-               socketS.receive(new DatagramPacket(receive, receive.length));
-               int puerto = Integer.parseInt(data(receive).toString());
-               System.out.println("Confirmación de :"+ puerto);
+            iniciarDiscos(socketS, adress);
+            
+            guardarLibroEnDiscos(socketS, adress, "descomprido.txt");
+            
+            recuperarArchivosDeDiscos(socketS, adress, "temp");
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } // TODO Auto-generated catch block
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public static void recuperarArchivosDeDiscos(DatagramSocket socketS,InetAddress adress,String name) throws IOException{
+            File temporal = new File("temporalFinal.txt");
+            temporal.createNewFile();
+            for (int i = 0; i < puertosDiscos.length; i++) {
+                // String name = dividirArchivo(i,dfll,dflel, 4, "descomprimido.txt");
+                byte[] accion = "recuperar".getBytes();
+                socketS.send(new DatagramPacket(accion, accion.length, adress, puertosDiscos[i]));
+
+                byte[] data2 = name.getBytes();
+                socketS.send(new DatagramPacket(data2, data2.length, adress, puertosDiscos[i]));
+
+                byte[] fileSize = new byte[1024];
+                socketS.receive(new DatagramPacket(fileSize, fileSize.length));
+                int tamaño = Integer.parseInt(data(fileSize).toString());
+                byte[] fileContent = new byte[tamaño];
+                socketS.receive(new DatagramPacket(fileContent, fileContent.length));
+                Files.write(new File("tempFile.txt").toPath(), fileContent);
+                byte[] srcPos = new byte[1024];
+                socketS.receive(new DatagramPacket(srcPos, srcPos.length));
+                int pos = Integer.parseInt(data(srcPos).toString());
+                armarArchivo(i, pos);
             }
-            String srcFilePath = "descomprimido.txt";
+    }
+    
+    public static void guardarLibroEnDiscos(DatagramSocket socketS,InetAddress adress, String srcFilePath) throws IOException{
             long srcFileLength = new File(srcFilePath).length();
             long dflel = srcFileLength / 4;
             long dfll = dflel + srcFileLength % 4;
-        
-            for(int i=0 ;i< puertosDiscos.length;i++){
-                String name = dividirArchivo(i,dfll,dflel, 4, "descomprimido.txt");
+
+            for (int i = 0; i < puertosDiscos.length; i++) {
+                byte[] accion = "guardar".getBytes();
+                socketS.send(new DatagramPacket(accion, accion.length, adress, puertosDiscos[i]));
+                String name = dividirArchivo(i, dfll, dflel, 4, "descomprimido.txt");
                 byte[] data2 = name.getBytes();
                 socketS.send(new DatagramPacket(data2, data2.length, adress, puertosDiscos[i]));
                 File file = new File(name);
@@ -45,96 +80,107 @@ public class UDPClient {
                 byte[] data3 = dataSize.getBytes();
                 socketS.send(new DatagramPacket(data3, data3.length, adress, puertosDiscos[i]));
                 socketS.send(new DatagramPacket(fileContent, fileContent.length, adress, puertosDiscos[i]));
-                
+
                 byte[] receive = new byte[65535];
                 socketS.receive(new DatagramPacket(receive, receive.length));
                 int puerto = Integer.parseInt(data(receive).toString());
-                System.out.println("Confirmación de dos :"+ puerto);
+                System.out.println("Confirmación de dos :" + puerto);
             }
-            
-            //DatagramSocket socketC = new DatagramSocket(8877);
-            System.out.println("Username: 8877");
-            System.out.println("Please enter the other party's IP, the other party's port, and the content sent, separated by spaces (such as: 189.163.122.122 8801 Hello). Press enter to send.");
-//            while (isNot) {
-//                String mensaje = "Hola soy un mensaje para el servidor";
-//                byte[] fasong = mensaje.getBytes();
-//                InetAddress address = InetAddress.getByName("localhost");
-//                int port2 = 8877;
-//                byte[] data2 = "Hello, I am the server, the connection is successful".getBytes();
-//                System.out.println("Client Port:" + port2);
-//                socketC.send(new DatagramPacket(data2, data2.length, address, 8888));
-//                if (str.equals("t")) {
-//                    isNot = false;
-//                } else {
-//                    byte[] data = str.getBytes();
-//                    //Create data package
-//                    DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
-//                    //Create DatagramSocket
-//                    socket.send(packet);
-//                }
-//            }
-        }catch (SocketException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        // TODO Auto-generated catch block
-         catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    }
+    
+    public static void iniciarDiscos(DatagramSocket socketS, InetAddress adress) throws IOException{
+        int numDisk = 4; 
+        puertosDiscos = new int[4];
+        int puertoBase = 8877;
+        for (int i = 0; i < numDisk; i++) {
+                puertosDiscos[i]=puertoBase;
+                byte[] data2 = String.valueOf(puertosDiscos[i]).getBytes();
+                System.out.println("Client Port:" + puertosDiscos[i]);
+                socketS.send(new DatagramPacket(data2, data2.length, adress, 7777));
+                byte[] receive = new byte[65535];
+                socketS.receive(new DatagramPacket(receive, receive.length));
+                int puerto = Integer.parseInt(data(receive).toString());
+                System.out.println("Confirmación de :" + puerto);
+                puertoBase++;
         }
     }
 
+    public static void armarArchivo(int loc, int srcPos) {
 
-    public static String dividirArchivo(int loc, long dfll,long dflel, int dataDisk, String srcFilePath) {
-		// long dfll;
-		long fileLength;// 分割后的文件长度
-		long startPos = loc * dflel;
-		if (loc != dataDisk - 1) {
-			fileLength = dflel;
-		} else {
-			fileLength = dfll;
-		}
-		File desFile = new File("temp" + "-" + startPos + ".txt");// 目标文件路径
-		if (desFile.exists()) {// 存在则删除
-			desFile.delete();
-		}
-		RandomAccessFile rafSrc = null;
-		RandomAccessFile rafDes = null;
+        RandomAccessFile rafSrc = null;
+        RandomAccessFile rafDes = null;
+        
+        try {
+            rafSrc = new RandomAccessFile("tempFile.txt", "r");
+            rafDes = new RandomAccessFile("temporalFinal.txt", "rw");
+            rafDes.seek(srcPos);// 设置文件指针位置
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = rafSrc.read(buffer)) != -1) {
+                rafDes.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rafDes.close();// 关闭流
+                rafSrc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		try {
-			desFile.createNewFile();// 创建文件
-			rafSrc = new RandomAccessFile(srcFilePath, "r");// 随机读方式打开
-			rafDes = new RandomAccessFile(desFile, "rw");// 随机写方式打开
-			rafSrc.seek(loc * dflel);// 设置读文件指针位置
-			int bufferLen = 1024;
-			byte[] buffer = new byte[bufferLen];
+    public static String dividirArchivo(int loc, long dfll, long dflel, int dataDisk, String srcFilePath) {
+        // long dfll;
+        long fileLength;// 分割后的文件长度
+        long startPos = loc * dflel;
+        if (loc != dataDisk - 1) {
+            fileLength = dflel;
+        } else {
+            fileLength = dfll;
+        }
+        File desFile = new File("temp" + "-" + startPos + ".txt");// 目标文件路径
+        if (desFile.exists()) {// 存在则删除
+            desFile.delete();
+        }
+        RandomAccessFile rafSrc = null;
+        RandomAccessFile rafDes = null;
 
-			if (fileLength <= bufferLen) {// 一次读出来的字节数大于该文件条带的大小
-				rafSrc.read(buffer);
-				rafDes.write(buffer, 0, (int) fileLength);
-			} else {
-				while (true) {
-					rafDes.write(buffer, 0, rafSrc.read(buffer));
-					fileLength -= bufferLen;
-					if (fileLength >= bufferLen) {// 剩余未读长度大于缓冲区长度
-						continue;
-					} else {
-						rafSrc.read(buffer);
-						rafDes.write(buffer, 0, (int) fileLength);
-						break;
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				rafDes.close();// 关闭流
-				rafSrc.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+        try {
+            desFile.createNewFile();// 创建文件
+            rafSrc = new RandomAccessFile(srcFilePath, "r");// 随机读方式打开
+            rafDes = new RandomAccessFile(desFile, "rw");// 随机写方式打开
+            rafSrc.seek(loc * dflel);// 设置读文件指针位置
+            int bufferLen = 1024;
+            byte[] buffer = new byte[bufferLen];
+
+            if (fileLength <= bufferLen) {// 一次读出来的字节数大于该文件条带的大小
+                rafSrc.read(buffer);
+                rafDes.write(buffer, 0, (int) fileLength);
+            } else {
+                while (true) {
+                    rafDes.write(buffer, 0, rafSrc.read(buffer));
+                    fileLength -= bufferLen;
+                    if (fileLength >= bufferLen) {// 剩余未读长度大于缓冲区长度
+                        continue;
+                    } else {
+                        rafSrc.read(buffer);
+                        rafDes.write(buffer, 0, (int) fileLength);
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rafDes.close();// 关闭流
+                rafSrc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return desFile.getName();
-	}
+    }
 }
--- ControllerNode/src/prueba/UDPServerRunnable.java
+++ ControllerNode/src/prueba/UDPServerRunnable.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 
 public class UDPServerRunnable extends Thread {
@@ -37,48 +38,75 @@ public class UDPServerRunnable extends Thread {
 
             while (true) {
 
-                byte[] data = new byte[1024];
+                byte[] accion = new byte[1024];
+                System.out.println("Disco " + this.discoNum + "esperando acciones");
+                disco.receive(new DatagramPacket(accion, accion.length));// Save data to packet
+                String accionString = data(accion).toString();
 
-                System.out.println("I am the server, waiting for the client to connect");
-                disco.receive(new DatagramPacket(data, data.length));// Save data to packet
-                
-                String name = data(data).toString();
-                System.out.println("Archivo: " + name);
-                byte[] dataSize = new byte[1024];
-                disco.receive(new DatagramPacket(dataSize, dataSize.length));// Save data to packet
-                int size = Integer.parseInt(data(dataSize).toString());
-                byte[] fileContent = new byte[size];
-                disco.receive(new DatagramPacket(fileContent, fileContent.length));// Save data to packet
+                switch (accionString) {
+                    case "guardar":
+                        GuardarArchivo(disco, puerto, discoNum);
+                        break;
+                    case "recuperar":
+                        byte[] nombreBytes = new byte[1024];
+                        disco.receive(new DatagramPacket(accion, accion.length));// Save data to packet
+                        String nombre = data(nombreBytes).toString();
+                        System.out.println("Disco " + this.discoNum + "recuperando archivo:" + nombre);
+                        RecuperarArchivo(disco, puerto, discoNum, nombre);
+                        break;
 
-                Files.write(new File("disk/raid5/disk" + this.discoNum + "/" + name).toPath(), fileContent);
-                byte[] data2 = String.valueOf(puerto).getBytes();
-                disco.send(new DatagramPacket(data2, data2.length, InetAddress.getByName("localhost"), 8888));
-                /*
-				 * Respond to the client
-                 */
-                // The information to be sent
-                // Define client address, port
-                // byte[] fasong = strings[2].getBytes();
-                // InetAddress address = InetAddress.getByName("localhost");
-                // int port2 = Integer.parseInt(strings[1]);
-                // byte[] data2 = "Hello, I am the server, the connection is
-                // successful".getBytes();
-                // System.out.println("Client Port:" + port2);
-                // DatagramPacket packet2 = new DatagramPacket(data2, data2.length, address,
-                // 8888);
-                // socket.send(packet2);
+                    default:
+                        break;
+                }
+
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         // TODO Auto-generated catch block
-         finally {
+        finally {
             if (socket != null) {
                 socket.close();
             }
         }
 
+    }
+
+    public static void GuardarArchivo(DatagramSocket disco, int puerto, int discoNum) throws IOException {
+        System.out.println("Disco " + discoNum + " Guardando Archivo...");
+        byte[] nombre = new byte[1024];
+        disco.receive(new DatagramPacket(nombre, nombre.length));// Save data to packet
+        String nombreString = data(nombre).toString();
+        System.out.println("Archivo: " + nombreString);
+        byte[] dataSize = new byte[1024];
+        disco.receive(new DatagramPacket(dataSize, dataSize.length));// Save data to packet
+        int size = Integer.parseInt(data(dataSize).toString());
+        byte[] fileContent = new byte[size];
+        disco.receive(new DatagramPacket(fileContent, fileContent.length));// Save data to packet
+        Files.write(new File("disk/raid5/disk" + discoNum + "/" + nombreString).toPath(), fileContent);
+        byte[] data2 = String.valueOf(puerto).getBytes();
+        disco.send(new DatagramPacket(data2, data2.length, InetAddress.getByName("localhost"), 8888));
+    }
+
+    public static void RecuperarArchivo(DatagramSocket disco, int puerto, int discoNum, String nombre) throws UnknownHostException, IOException {
+        String filesName[] = new File("disk/raid5/disk" + discoNum + "/").list();
+        String archivo = "";
+        for (int i = 0; i < filesName.length; i++) {
+            if (filesName[i].matches(".*" + nombre + "-\\d+" + ".txt")) {
+                archivo = filesName[i];
+                break;
+            }
+        }
+        File file = new File(archivo);
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+        String dataSize = String.valueOf(fileContent.length);
+        byte[] data3 = dataSize.getBytes();
+        String srcPos = archivo.substring(archivo.indexOf("-") + 1,archivo.indexOf("."));
+
+        disco.send(new DatagramPacket(data3, data3.length,InetAddress.getByName("localhost"), 8888));
+        disco.send(new DatagramPacket(fileContent, fileContent.length,InetAddress.getByName("localhost"), 8888));
+        disco.send(new DatagramPacket(srcPos.getBytes(), srcPos.getBytes().length, InetAddress.getByName("localhost"), 8888));
     }
 
     public static StringBuilder data(byte[] a) {
